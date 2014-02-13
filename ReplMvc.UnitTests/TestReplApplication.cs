ddc695a55f2a0fd12cf20a11c0f8d7a0b6df1515
Jake Bladt <jake@jakebladt.com>
2014-02-13 16:03:12 -0500

    Added test project

---
+++ ReplMvc.UnitTests/TestReplApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ReplMvc;
+
+namespace ReplMvc.UnitTests
+{
+    [TestClass]
+    public class TestReplApplication
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ReplMvc/ActionResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReplMvc
+{
+    public class ActionResult
+    {
+        public bool Success { get; set; }
+        public bool IsTerminalAction { get; set; }
+        public string[] Messages { get; set; }
+
+        public ActionResult(string[] msgs, bool success, bool isTerminal = false)
+        {
+            Messages = msgs;
+            Success = success;
+            IsTerminalAction = isTerminal;
+        }
+    }
+}
