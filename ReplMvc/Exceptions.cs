6e3ead0f2463bf60848bc80776753d33e73f8ee5
Jake Bladt <jake@jakebladt.com>
2014-02-15 20:58:01 -0500

    Added missing exceptions file.

---
+++ ReplMvc/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplMvc.Exceptions
+{
+    public class ReplAppSetupException : System.Exception
+    {
+        public ReplAppSetupException() : base() { }
+        public ReplAppSetupException(string msg) : base(msg) { }
+    }
+
+    public class NoViewRegisteredException : System.Exception
+    {
+        public NoViewRegisteredException() : base() { }
+        public NoViewRegisteredException(string msg) : base(msg) { }
+    }
+}
